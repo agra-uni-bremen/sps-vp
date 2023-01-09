5c7dc1d13410579310df0a1dc3acf26e0d5c5827
Sören Tempel <tempel@uni-bremen.de>
2023-01-09 22:37:43 +0100

    Adjust copyright years in some files

--- vp/src/symex/symbolic_format.h
+++ vp/src/symex/symbolic_format.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Group of Computer Architecture, University of Bremen
+ * Copyright (c) 2021,2022 Group of Computer Architecture, University of Bremen
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
